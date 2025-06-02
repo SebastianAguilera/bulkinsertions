ff71d72754b0c88252400a0c19a152b3e46d7b76
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2025-06-02 17:44:52 -0300

    se agrega instancia migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from app.config import config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app() -> Flask:
     """
@@ -16,7 +18,9 @@ def create_app() -> Flask:
     app = Flask(__name__)
     f = config.factory(app_context if app_context else 'development')
     app.config.from_object(f)
+    
     db.init_app(app)
+    migrate.init_app(app, db)
 
     @app.shell_context_processor    
     def ctx():
